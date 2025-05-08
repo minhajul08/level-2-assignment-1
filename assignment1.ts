bcef7dd069cf81ea047a9fd94fb0888823a73d60
minhajul08 <minhazmehadi61@gmail.com>
2025-05-08 22:24:34 +0600

    problem - 8 sloved

--- assignment1.ts
+++ assignment1.ts
@@ -143,6 +143,25 @@ function getDayType(day: Day): string {
 // console.log(getDayType(Day.Tuesday));   
 
 
+// Problem -8
+
+async function squareAsync(n: number): Promise<number> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (n < 0) {
+        reject(new Error("Negative number not allowed"));
+      } else {
+        resolve(n * n);
+      }
+    }, 1000);
+  });
+}
+
+
+squareAsync(4).then(console.log);      
+// squareAsync(-3).catch(console.error); 
+
+
 
 
 
